913ec95a65394d67533d16f112de0710fe0f7f87
Ben Kreeger <ben@kree.gr>
2013-08-27 18:31:07 -0500

    Removing unnecessary migrations, filling out existing ones.

--- db/migrate/20130827232005_create_feeds.rb
+++ db/migrate/20130827232005_create_feeds.rb
@@ -1,7 +1,7 @@
 class CreateFeeds < ActiveRecord::Migration
   def change
     create_table :feeds do |t|
-      t.string :name
+      t.string :name, index: true
       t.string :uri
       t.string :mime_type
       t.timestamps
--- db/migrate/20130827232241_create_fetches.rb
+++ db/migrate/20130827232241_create_fetches.rb
@@ -1,8 +1,10 @@
 class CreateFetches < ActiveRecord::Migration
   def change
     create_table :fetches do |t|
-
-      t.timestamps
+      t.integer :feed_id
+      t.foreign_key :feed
+      t.string :document_id, index: true
+      t.datetime :created_at
     end
   end
 end

49cadba803cc8809e2a3171f4b3f773116b7eef9
Ben Kreeger <ben@kree.gr>
2013-08-27 18:33:40 -0500

    Fixing migrations, running them.

--- app/models/fetch.rb
+++ app/models/fetch.rb
@@ -1,2 +1,3 @@
 class Fetch < ActiveRecord::Base
+  belongs_to :feed
 end
--- db/migrate/20130827232241_create_fetches.rb
+++ db/migrate/20130827232241_create_fetches.rb
@@ -2,7 +2,7 @@ class CreateFetches < ActiveRecord::Migration
   def change
     create_table :fetches do |t|
       t.integer :feed_id
-      t.foreign_key :feed
+      t.foreign_key :feeds
       t.string :document_id, index: true
       t.datetime :created_at
     end
